6fe783e4fc6f5dc8ec4b0b3bdbb8e3f425f3765b
yurihs <yurisalvador@hotmail.com>
2017-12-08 18:18:25 -0200

    Improve variable names and triangle documentation

--- graphics_extra/triangle.py
+++ graphics_extra/triangle.py
@@ -2,62 +2,117 @@ import trianglesolver
 import math
 import graphics
 from collections import namedtuple
+from typing import Optional, Tuple
 
-Medidas = namedtuple('Medidas', 'lado_a lado_b lado_c angulo_a angulo_b angulo_c')
-
+Measurements = namedtuple(
+    'Measurements',
+    'side_a side_b side_c angle_a angle_b angle_c'
+)
 
 class Triangle(graphics.GraphicsObject):
-    def __init__(self, vertice_b, **dados):
+    """
+    Desenha um triangulo, conforme os dados fornecidos.
+
+    É NECESSÁRIO FORNECER AO MENOS 3 das seguintes medidas para definir um
+    triângulo válido.
+    SIDE (a, b, c): Comprimento dos lados
+    ANGLE (a, b, c): Ângulos internos (EM RADIANOS)
+
+    O posicionamento do triângulo é definido pela posição do vértice (vertex) B.
+
+            C
+            |\
+            | \
+            |  \
+            |   \
+     side_a |    \ side_b
+            |     \
+            |      \
+            |_______\
+           B  side_c  A
+
+    Parametros:
+        vertex_b: Vértice b. Define a posição do triângulo na tela.
+                  Os outros vértice serão calculados automaticamente,
+                  e só podem ser mudados indiretamente, ao alterar as outras
+                  medidas do triângulo.
+        data: AO MENOS 3 das medidas necessárias para definir um triângulo
+              (comprimento dos lados ou angulos internos EM RADIANOS).
+              Qualquer desses: lado_a, lado_b, lado_c,
+                               angulo_a, angulo_b, angulo_c
+
+
+    """
+    def __init__(self, vertex_b: graphics.Point, **data):
         super().__init__(options=["outline", "width", "fill"])
-        self.vertice_b = vertice_b
-        self.dados = dict(dados)
+        self.vertex_b = vertex_b
+        # Dados serão processados depois, para completar as medidas do triângulo
+        self._data = dict(data)
 
-    def __repr__(self):
-        return 'Triangle()'.format()
+    def __repr__(self) -> str:
+        description = []
+        for k, v in self.measurements._asdict().items():
+            if v is not None:
+                if k.startswith('angle_'):
+                    # Mostrar os ângulos em graus
+                    description.append('{}={:.2f}°'.format(k, math.degrees(v)))
+                else:
+                    description.append('{}={:.2f}'.format(k, v))
 
-    def _move(self, dx, dy):
-        self.vertice_b._move(dx, dy)
+        return 'Triangle({})'.format(', '.join(description))
 
-    def _draw(self, canvas, options):
+    def _move(self, dx: Optional[int] = None, dy: Optional[int] = None):
+        # O triângulo é movido com relação ao vértice B.
+        self.vertex_b._move(dx, dy)
+
+    def _draw(self, canvas: graphics.GraphWin, options) -> int:
+        # Converter as posições dos vértices em posições reais na tela (pixels)
         vertices = [(canvas.toScreen(p.getX(), p.getY())) for p in self.vertices]
-        return canvas.create_polygon(*vertices)
+        # Desenhar como um polígono
+        return canvas.create_polygon(*vertices, options)
 
     @property
-    def medidas(self):
+    def measurements(self) -> Measurements:
+        """
+        Calcular as medidas absolutas do triângulo,
+        a partir dos dados incompletos fornecidos.
+        """
         a, b, c, aA, aB, aC = trianglesolver.solve(
-            a=self.dados.get('lado_a'),
-            b=self.dados.get('lado_b'),
-            c=self.dados.get('lado_c'),
-            A=self.dados.get('angulo_a'),
-            B=self.dados.get('angulo_b'),
-            C=self.dados.get('angulo_c')
+            a=self._data.get('side_a'),
+            b=self._data.get('side_b'),
+            c=self._data.get('side_c'),
+            A=self._data.get('angle_a'),
+            B=self._data.get('angle_b'),
+            C=self._data.get('angle_c')
         )
-        return Medidas(lado_a=a, lado_b=b, lado_c=c, angulo_a=aA, angulo_b=aB, angulo_c=aC)
-
-    @property
-    def vertice_c(self):
-        return graphics.Point(
-            self.vertice_b.getX() + self.medidas.lado_a * math.cos(self.medidas.angulo_b),
-            self.vertice_b.getY() - self.medidas.lado_a * math.sin(self.medidas.angulo_b)
+        return Measurements(
+            side_a=a, side_b=b, side_c=c, angle_a=aA, angle_b=aB, angle_c=aC
         )
 
     @property
-    def vertice_a(self):
-        return graphics.Point(
-            self.vertice_b.getX() + self.medidas.lado_c,
-            self.vertice_b.getY()
-        )
-
-    @property
-    def vertices(self):
+    def vertex_a(self) -> graphics.Point:
         """
-        C
-          |\
-          | \
-        a |  \ b
-          |   \
-          |____\
-        B    c   A
-
+        Calcular a posiçao do vértice A, partindo do vértice B (linha reta).
         """
-        return (self.vertice_a, self.vertice_b, self.vertice_c)
+        return graphics.Point(
+            self.vertex_b.getX() + self.measurements.side_c,
+            self.vertex_b.getY()
+        )
+
+    @property
+    def vertex_c(self) -> graphics.Point:
+        """
+        Calcular a posição do vértice C, partindo do vértice B.
+        Fórmula de: https://stackoverflow.com/a/1571322
+        """
+        return graphics.Point(
+            self.vertex_b.getX() + self.measurements.side_a * math.cos(self.measurements.angle_b),
+            self.vertex_b.getY() - self.measurements.side_a * math.sin(self.measurements.angle_b)
+        )
+
+    @property
+    def vertices(self) -> Tuple[graphics.Point, graphics.Point, graphics.Point]:
+        """
+        Retorna os três vértices do triângulo, que o definem absolutamente.
+        """
+        return (self.vertex_a, self.vertex_b, self.vertex_c)
