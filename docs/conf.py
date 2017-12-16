e2b77a58155fcda341f3639b3c62028408b3fa9b
yurihs <yurisalvador@hotmail.com>
2017-12-15 22:21:54 -0200

    Change docs ext config order

--- docs/conf.py
+++ docs/conf.py
@@ -32,10 +32,11 @@ sys.path.insert(0, os.path.abspath('..'))
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'sphinx_autodoc_napoleon_typehints']
-
 # Fixes ReadTheDocs build (no display there, tkinter errors out)
 autodoc_mock_imports = ['_tkinter']
+
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'sphinx_autodoc_napoleon_typehints']
+
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
